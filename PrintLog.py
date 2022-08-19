dd0cc373ff9be2e1ad004fd41c31b8295dda06fa
yyyljy <yyyljy@gmail.com>
2022-08-19 13:57:21 +0900

    writing on web update

--- PrintLog.py
+++ PrintLog.py
@@ -1,4 +1,37 @@
+import imp
 import time
+import sys
+import requests
+from urllib.request import urlopen
+import json
 
 def pLog(str):
-    print(time.strftime("%Y-%m-%d %H:%M:%S") + "\t" + str)
+    timenow = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(timenow + "\t" + str)
+    writeArticleOnWeb(timenow, str)
+
+def writeArticleOnWeb(title, contents):
+    url = 'http://yuniverse.me/articles/write/'
+    client = requests.session()
+
+    while(True):
+        try:
+            client.get(url)  # sets cookie
+        
+        except:
+            page = urlopen(url)
+            doc = page.read().decode('utf-8')
+            dic = json.loads(doc)
+        
+        else:
+            if 'csrftoken' in client.cookies:
+                # Django 1.6 and up
+                csrftoken = client.cookies['csrftoken']
+            else:
+                # older versions
+                csrftoken = client.cookies['csrf']
+
+            write_url = 'http://yuniverse.me/articles/create/'
+            data = dict(title=title, contents=contents, writer='UpbitAPI', board_name='Trading', csrfmiddlewaretoken=csrftoken, next='/')
+            req = client.post(write_url, data=data, headers=dict(Referer=url))
+            return req
